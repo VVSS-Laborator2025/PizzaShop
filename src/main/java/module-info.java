c9cd83db3338d10ac2c6e45ff0d6f9190195afde
zdrancrares <zdrancrares@gmail.com>
2025-04-29 21:53:41 +0300

    lab4

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,4 +11,6 @@ module pizzashop {
     exports pizzashop;
     opens pizzashop.controller to javafx.fxml;
     exports pizzashop.controller;
+
+    opens pizzashop.service to org.junit.platform.commons;
 }
